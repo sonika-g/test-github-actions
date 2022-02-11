2a5f33b7109f6df4877d63776bc4d272bd6e1f09
Sonika Gupta <sonika.gupta@everest.engineering>
2022-02-11 12:28:02 +0530

    add new script to test variable

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,24 +1,32 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
+# mkdir -p ../.ssh
+# echo "$SSH_PRIVATE_KEY" > ../.ssh/id_rsa
+# chmod 400 ../.ssh/id_rsa
 
-echo "Set up ssh"
-mkdir -p ~/.ssh
-echo $1 > ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
-echo "File created"
-chmod 400 ~/.ssh/id_rsa
-echo "Permission Changed"
+# echo "Host server_instance
+# HostName $host
+# StrictHostKeyChecking no
+# User ec2-user
+# IdentityFile ../.ssh/id_rsa
+# ProxyCommand none" > ../.ssh/config
 
-echo "Set up ssh config"
-echo "Host $2
-HostName server
-StrictHostKeyChecking no
-User ec2-user
-IdentityFile ~/.ssh/id_rsa
-ProxyCommand none" > ~/.ssh/config
+# if [[ $1 == "database" ]]; then
+#     # private_ip=$(aws ssm get-parameters --names "$parameter_name" --query "Parameters[*].Value" --output text --debug)
+#     echo "Host database_instance
+#     HostName $private_ip
+#     StrictHostKeyChecking no
+#     User ec2-user
+#     IdentityFile ../.ssh/id_rsa
+#     ProxyCommand ssh timesheets_server_instance -W %h:%p'" > ../.ssh/config
+#     echo "[$group]
+#     database_instance" > ../inventory
+# else
+#     echo "[$group] 
+#     server_instance" > ../inventory
+# fi
 
-echo "Create Inventory File"
-echo "[$3] 
-server" > ~/inventory
+# cat ../inventory
+# cat ../.ssh/config
+
+echo "hi" > ./text
+echo "hello" >> ./text
+cat ./text
